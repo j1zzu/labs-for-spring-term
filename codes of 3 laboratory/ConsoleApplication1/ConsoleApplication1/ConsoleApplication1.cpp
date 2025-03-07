745a0eccd3f4c30ecbecad7a26149d270384efe4
Rozhin Kirill <101795751+j1zzu@users.noreply.github.com>
2025-03-07 14:05:35 +0300

    Add files via upload

---
+++ codes of 3 laboratory/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>;
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    string str_numbers;
+    int addition = 0;
+    int second_number;
+    cout << "Введите строку, содержащую числовое значение: ";
+    cin >> str_numbers;
+    int integer_numbers = stoi(str_numbers);
+    cout << "Введите второе число для сложения: ";
+    cin >> second_number;
+    addition = integer_numbers + second_number;
+    cout << "Сумма чисел: " << addition;
+}
