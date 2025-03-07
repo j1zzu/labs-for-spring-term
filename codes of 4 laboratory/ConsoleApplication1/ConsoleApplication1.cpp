afc5dda3799f024fcc5911b5c87f6f026c372b64
Rozhin Kirill <101795751+j1zzu@users.noreply.github.com>
2025-03-07 14:03:50 +0300

    Add files via upload

---
+++ codes of 4 laboratory/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm> //подключаем для transform
+
+int main () {
+	//Программа, в которой составляется предложение с прописными буквами на английском языке с использованием двух переменных, после чего происходит замена прописных букв на строчные
+
+	system("chcp 1251");
+	std::string first_name;
+	std::string second_name;
+
+	std::cout << "Введите первую часть предложения прописными буквами на английском языке: ";
+	getline(std::cin,first_name);
+
+	std::cout << "Введите вторую часть предложения прописными буквами на английском языке: ";
+	getline(std::cin, second_name);
+
+	//Составленное предложение c прописными буквами на английском языке
+	std::string sentence = first_name + ' ' + second_name; 
+	std::cout << "Полученное предложение: " << sentence << std::endl;
+
+	//Преобразование предложения из прописных букв в строчные буквы
+	transform(sentence.begin(), sentence.end(), sentence.begin(), ::tolower);
+	
+	std::cout << "Предложение со строковыми буквами: " << sentence;
+	return 0;
+}
