08caaab51278911e6a15242615c07d225f1db055
Rozhin Kirill <101795751+j1zzu@users.noreply.github.com>
2025-03-09 12:19:18 +0300

    Add files via upload

---
+++ codes of 7 laboratory/function/function/function.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+template <typename number_type>
+
+number_type max(const number_type& x, const number_type& y) {
+    if (x > y) {
+        return x;
+    }
+    else {
+        return y;
+    }
+}
+
+int main()
+{
+    float x = 2.4;
+    float y = 14.2;
+    std::cout << "Наибольшее из чисел:" << max(x, y);
+}
---
+++ codes of 8 laboratory/codes of 8 laboratory/codes of 8 laboratory.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+const double pi = 3.14159265358979323846;
+
+int main()
+{
+    system("chcp 1251");
+
+    int angle;
+    double radians;
+
+    std::cout << "Введите угол. Вычислится его синус, косинус и тангенс: ";
+    std::cin >> angle;
+
+
+    radians = angle * (pi / 180); //преобразуем градусы в радианы
+
+    std::cout << "Результаты вычислений: " << "Синус равен:" << sin(radians) << " Косинус равен: " << cos(radians) << " Тангенс равен: " << tan(radians);
+}
+
